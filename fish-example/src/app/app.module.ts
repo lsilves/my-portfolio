4a55c3428b23c77fc9f5c030880ed7f8cdb965f6
Laura I <lsilves@myseneca.ca>
2020-05-20 21:59:51 -0400

    removed map

--- fish-example/src/app/app.module.ts
+++ fish-example/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { MapComponent } from './map/map.component';
-import { AgmCoreModule } from '@agm/core';
 import { SunflowerComponent } from './sunflower/sunflower.component';
 import { WebpageDesignsComponent } from './webpage-designs/webpage-designs.component';
 import { AssortedGraphicsComponent } from './assorted-graphics/assorted-graphics.component';
@@ -16,21 +14,13 @@ import { AssortedGraphicsComponent } from './assorted-graphics/assorted-graphics
     AppComponent,
     HomeComponent,
     NavigationBarComponent,
-    MapComponent,
     SunflowerComponent,
     WebpageDesignsComponent,
     AssortedGraphicsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,    
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCh9Ohav8Vr7vn90fwTSXHfoeKAa83cTSg'
-      /* apiKey is required, unless you are a
-      premium customer, in which case you can
-      use clientId
-      */
-    })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
